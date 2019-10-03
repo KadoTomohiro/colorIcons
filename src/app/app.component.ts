b754c4e7cc187b5c11275986da8afea0c0648ad3
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2019-10-03 11:41:39 +0900

    アイコンを選択し、色を変える

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {iconNames} from './iconNames';
 
 @Component({
   selector: 'ci-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'colorIcons';
+
+  color: string;
+  name: string = 'home';
+
+  iconNames: string[] = iconNames;
 }
